da7ea960bc1f72c61728b4ffe4f17448ec536f75
minoritydev <30841345+minoritydev@users.noreply.github.com>
2017-09-22 00:11:58 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,14 +2,6 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 var app = express();
-var Pool = require('pg');
-var connection = "postgresql://debarghyas:db-debarghyas-30817@db.imad.hasura-app.io:5432/debarghyas";
-app.use(morgan('combined'));
-const pool = new Pool({
-    connection: connection
-});
-Pool.connect();
-
 
 app.get('/', function (req, res) {
 res.sendFile(path.join(__dirname, 'ui', 'index.html'));
@@ -28,8 +20,5 @@ res.sendFile(path.join(__dirname, 'ui' , 'list.html'));
 
 }); 
 
-app.get('/testdb', function(req, res){
-    pool.query('SELECT * FROM List');
-    pool.end();
-});
+
 console.log ('IMAD course app listening on port ${port}!');
