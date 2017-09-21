dac95042cd607952f3ae605195766d9e076aab34
minoritydev <30841345+minoritydev@users.noreply.github.com>
2017-09-21 22:45:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-var Pool = require('pg').Pool;
+const {Pool, Client} = require('pg');
 
-var config = {
-user: 'debarghyas' ,
-database: 'debarghyas' ,
-host: 'db.imad.hasura-app.io' ,
-port: '5432' , 
-password: 'db-debarghyas-30817'
-};
-
+const pool = new Pool({
+    user: 'debarghyas',
+    host: 'db.imad.hasura-app.io',
+    database: 'debarghyas',
+    password: process.env.DB_PASSWORD,
+    port: 5432
+});
 var app = express();
 app.use(morgan('combined'));
 
@@ -30,20 +29,12 @@ app.get('/list', function (req,res){
 res.sendFile(path.join(__dirname, 'ui' , 'list.html'));
 
 }); 
- Pool = new Pool(config);
 
-app.get('/testdb', function (req, res){
-
-Pool.query('select * from List', function (err, result) {
-if (err) { res.status(404).send(err, toString());}
-else {res.send(JSON.stringfy(result));}
+app.get('/testdb', function(req, res){
+    pool.query('SELECT * FROM List');
+    console.log (err.res);
+    pool.end();
 });
-
-});
-// Do not change port, otherwise your app won't run on IMAD servers
-// Use 8080 only for local development if you already have apache running on 80
-
-var port = 80;
-app.listen(port, function () {
+ 
 console.log ('IMAD course app listening on port ${port}!');
 });
